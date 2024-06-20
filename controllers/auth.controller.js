26adbe8b7fa3c6b987a1f434d04c5b31763a0f9e
Youngho Park <dudgh4651@gmail.com>
2024-06-20 23:01:34 +0900

    [feat] 기본세팅

---
+++ controllers/auth.controller.js
@@ -0,0 +1,21 @@
+const { response } = require('express');
+const User = require('../models/User');
+const bcrypt  = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY;
+
+const authController = {};
+
+authController.loginWithEmail = async (req,res) =>{
+}
+
+authController.authenticate = async (req, res, next) => {
+}
+
+authController.checkAdminPermission = async (req, res, next) => {
+}
+
+
+module.exports = authController;
---
+++ controllers/order.controller.js
@@ -0,0 +1,19 @@
+const Order = require("../models/Order");
+const productController = require("./product.controller");
+const { randomStringGenerator } = require("../utils/randomStringGenerator");
+
+const orderController = {};
+
+orderController.createOrder = async (req, res) => {
+};
+
+orderController.getOrder = async (req, res) => {
+};
+
+orderController.getOrderList = async (req, res) => {
+};
+
+orderController.updateOrder = async (req, res) => {
+};
+
+module.exports = orderController;
