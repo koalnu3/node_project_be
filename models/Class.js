6d9201af00ac05cc9c4bda35d0fa8e65c783fb73
Youngho Park <dudgh4651@gmail.com>
2024-06-28 00:34:41 +0900

    [feat] 클래스like증가, 클래스조회 정렬

--- models/Class.js
+++ models/Class.js
@@ -1,65 +1,84 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const User = require("./User");
 const Category = require("./Category");
 const Schema = mongoose.Schema;
-const classSchema = Schema({
-    id:{
-        type:String,
-        required:true,
-        unique:true
+
+const curriculumItemSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subItems: [subItemSchema],
+});
+
+const subItemSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const classSchema = Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
     },
     name: {
-        type:String,
-        required:true     
+      type: String,
+      required: true,
     },
     description: {
-        type:String,
-        required:true     
+      type: String,
+      required: true,
     },
     image: {
-        type:Array,
-        required:true     
+      type: Array,
+      required: true,
     },
     curriculum: {
-        type:Array,
-        required:true     
-    },    
+      type: [curriculumItemSchema],
+      required: true,
+    },
     price: {
-        type:Number,
-        required:true     
+      type: Number,
+      required: true,
     },
     notice: {
-        type:String,
-        required:true     
+      type: String,
+      required: true,
     },
     category: {
-        type:String,
-        required:true
+      type: String,
+      required: true,
     },
-    userId: { 
-        type: mongoose.ObjectId, 
-        ref: User
+    userId: {
+      type: mongoose.ObjectId,
+      ref: User,
     },
     status: {
-        type:String,
-        default:'request'     
+      type: String,
+      default: "request",
     },
-    isDeleted:{
-        type:Boolean,
-        default:false
+    isDeleted: {
+      type: Boolean,
+      default: false,
     },
     likes: {
-        type: Number,
-        default: 0
-    }
-}, {timestamps:true})
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
 classSchema.methods.toJSON = function () {
-    const obj = this._doc;
-    delete obj.__v;
-    delete obj.updateAt;
-    delete obj.createAt;
-    return obj;
-}
-const Class = mongoose.model('Class', classSchema)
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updateAt;
+  delete obj.createAt;
+  return obj;
+};
+const Class = mongoose.model("Class", classSchema);
 module.exports = Class;
